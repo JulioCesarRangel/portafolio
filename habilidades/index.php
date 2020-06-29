8398b43f7c9302629eb4ad7d0efe43945f967cac
JulioCesarRangel <67608791+JulioCesarRangel@users.noreply.github.com>
2020-06-29 18:45:06 -0400

    Update index.php

--- habilidades/index.php
+++ habilidades/index.php
@@ -9,9 +9,25 @@
     <title>Julio Rangel | Habilidades</title>
   </head>
   <body style="background:orange">
-    <?php
-      require('../nav/index.php');
-    ?>
+
+    <nav class="nav">
+      <input type="checkbox" id="checkm" onclick="return tap();">
+      <label  for="checkm" class="btn-menu hide-on-med-and-up right">
+        <i class="material-icons">menu</i>
+      </label>
+      <ul>
+        <label  for="checkm" class="right btn-menu hide-on-med-and-up">
+          <i class="material-icons">close</i>
+        </label>
+        <br class="hide-on-med-and-up">
+        <br class="hide-on-med-and-up">
+        <li><a href="/portafolio">Sobre mi</a></li>
+        <li class="activebtn"><a href="/portafolio/habilidades">Habilidades</a></li>
+        <li><a href="/portafolio/proyectos">Proyectos</a></li>
+        <li><a href="/portafolio/contacto">Contacto</a></li>
+      </ul>
+    </nav>
+    
   </body>
   <script type="text/javascript" src="../index.js"></script>
 </html>
